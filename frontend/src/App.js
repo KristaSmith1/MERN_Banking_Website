aa2ea99ee6b370557ffc0e4f5fb093737448deab
ashiggles <adamshigley@mail.weber.edu>
2024-07-24 22:17:20 -0600

    Added React and Admin account starter

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+//import Navbar from "./components/navbar";
+//import Welcome from "./components/homepage";
+import Account from "./components/account";
+ 
+const App = () => {
+ return (
+   <div>
+     {/* <Navbar /> */}
+     <Routes>
+        {/* <Route exact path="/" element={<Welcome />} /> */}
+        <Route path="/account" element={<Account />} />
+     </Routes>
+   </div>
+ );
+};
+ 
+export default App;
---
+++ frontend/src/components/account.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export  default function Account() {
+
+    return (
+        <div>
+            <h1>Administrator Account</h1>
+        </div>
+    );
+}
