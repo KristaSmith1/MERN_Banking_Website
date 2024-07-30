9f506982c443ea5bcdb002e8e3962ca6aaebf526
KristaSmith1 <122482815+KristaSmith1@users.noreply.github.com>
2024-07-29 18:40:55 -0600

    Update App.js
    Added login path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
- 
+
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- 
+
 // We import all the components we need in our app
 //import Navbar from "./components/navbar";
-//import Welcome from "./components/homepage";
+import Login from "./components/homepage";
 import Account from "./components/account";
- 
+
 const App = () => {
- return (
-   <div>
-     {/* <Navbar /> */}
-     <Routes>
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route exact path="/" element={<Login />} />
         {/* <Route exact path="/" element={<Welcome />} /> */}
         <Route path="/account" element={<Account />} />
-     </Routes>
-   </div>
- );
+      </Routes>
+    </div>
+  );
 };
- 
+
 export default App;
