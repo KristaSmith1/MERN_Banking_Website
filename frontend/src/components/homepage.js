801adeebe4ae857060d5afcbe84a7fb5a590e0d8
KristaSmith1 <122482815+KristaSmith1@users.noreply.github.com>
2024-07-31 19:52:57 -0600

    Update homepage.js
    Added an 'invalid email/password' message, fixed fetching problem

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -1,9 +1,9 @@
 // This file was created to make the frontend folder
 // the Login function will allow users to login
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 //import { NavLink } from "react-router-dom";
-//import { useNavigate } from "react-router";
+import { useParams, useNavigate } from "react-router";
 import { ReactSession } from "react-client-session";
 
 import "./homepage.css";
@@ -11,12 +11,14 @@ import "./homepage.css";
 export default function Login() {
     const [form, setForm] = useState({
         // set form to blanks as default
-        username: "",
+        email: "",
         password: "",
         message: ""
     });
 
-    //const navigate = useNavigate(); // allow user to navigate to other pages
+    const [invalidMessage, setInvalidMessage] = useState(''); // message to show incorrect username/password
+
+    const navigate = useNavigate(); // allow user to navigate to other pages
 
     function updateForm(value) {
         // update variables
@@ -29,29 +31,36 @@ export default function Login() {
         // when form is submitted, send info to db
         e.preventDefault();
 
-        const formData = { ...form };
+        const existingPerson = { ...form };
 
-        const response = await fetch("http://localhost:5000/bankAccounts", {
-            // send form data to the db
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-        }
-        )
+        const response = await fetch("http://localhost:5000/accounts/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(existingPerson),
+            })
             .catch(error => {
+                console.log("ERROR - UNABLE TO FETCH");
                 // display error message if unable to fetch info
                 window.alert(error);
                 return;
             });
 
         const user = await response.json();
+
+        console.log("User: " + user);
+        console.log("email: " + user.email);
+        console.log("password: " + user.password);
+        console.log("message: " + user.message);
+
         // reset form to blank, set message to username
-        setForm({ username: "", password: "", message: user.message });
+        setForm({ email: "", password: "", message: user.message });
+
+
 
         if (user.message == null) {
-            // if no
             ReactSession.setStoreType("sessionStorage");
             ReactSession.set("username", user._id);
 
@@ -60,26 +69,29 @@ export default function Login() {
 
 
             // navigate to user account
-            //navigate("/userAccount/" + user._id);
+            //console.log("About to navigate to account");
+            navigate("/account");
         }
 
-
+        setInvalidMessage("Invalid email or password. Please correct and try again.");
 
     }
 
     return (
         <div>
             <h3>Welcome to Gecko Banking!</h3>
-            <div class="card">
+            <div className="card">
                 <img src="https://images.unsplash.com/photo-1634207138281-3321b35e9662?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="A gecko"
                 />
-                <div class="card-body">
+                <div className="card-body">
                     <form onSubmit={onSubmit}>
 
-                        <label for="username" class="form-label">Username</label>
-                        <input type="text" id="usernameInput" class="form-control border-success" placeholder="Please enter your username"
-                            value={form.username}
-                            onChange={(e) => updateForm({ username: e.target.value })}
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="text" id="email"
+                            // class="form-control border-success"
+                            placeholder="Please enter your email"
+                            value={form.email}
+                            onChange={(e) => updateForm({ email: e.target.value })}
                         />
 
                         <div>
@@ -89,6 +101,7 @@ export default function Login() {
                                 onChange={(e) => updateForm({ password: e.target.value })}
                             />
                         </div>
+                        <div className="invalidMessage">{invalidMessage}</div>
                         <div>
                             <input type="submit" value="Login" className="submit-button" />
                         </div>
